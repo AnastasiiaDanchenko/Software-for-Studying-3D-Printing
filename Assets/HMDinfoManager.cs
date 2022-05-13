76da487bfdddba312402a947597825ecac4dc34c
Anastasiia Danchenko <addanchenko@miem.hse.ru>
2022-05-13 20:52:38 +0300

    Thesis: Assets 1

---
+++ Assets/HMDinfoManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDinfoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!XRSettings.isDeviceActive)
+        {
+            Debug.Log("No Headset plugged");
+        }
+        else if(XRSettings.isDeviceActive && (XRSettings.loadedDeviceName == "Mock HMD" || XRSettings.loadedDeviceName == "MockHMD Display"))
+        {
+            Debug.Log("Using Mock HMD");
+        }
+        else
+        {
+            Debug.Log("Using this Headset: " + XRSettings.loadedDeviceName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
